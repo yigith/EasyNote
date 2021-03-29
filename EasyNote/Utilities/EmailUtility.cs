2cd9f763fa5d8d2b50c61863146476c28f2fb2ab
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-03-29 15:53:53 +0300

    email service

---
+++ EasyNote/Utilities/EmailUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EasyNote.Utilities
+{
+    public static class EmailUtility
+    {
+        public static async Task SendEmailAsync(string to, string subject, string body)
+        {
+            using (var smtp = new SmtpClient("kod.fun"))
+            {
+                smtp.Credentials = new NetworkCredential("easynote@kod.fun", "*************");
+                smtp.EnableSsl = true;
+                smtp.Port = 587;
+                MailMessage message = new MailMessage("easynote@kod.fun", to, subject, body);
+                message.IsBodyHtml = true;
+                await smtp.SendMailAsync(message);
+            }
+        }
+    }
+}
